f1d99610bf22fa7ec8a6c43371ccfae62a8344b1
TuanHoMinhMinevich <62999588+StepTuan@users.noreply.github.com>
2021-06-07 04:31:12 +0300

    Add files via upload

---
+++ UnitTest4_3.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ALG_4_3/Func.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest43
+{
+	TEST_CLASS(UnitTest43)
+	{
+	public:
+		TEST_METHOD(data_set)
+		{
+			string a = "";
+			string expected = "Could not open file";
+			ifstream in;
+			if (!in.is_open()) a = "Could not open file";
+			Assert::AreEqual(expected, a);
+		}
+
+		TEST_METHOD(dataprice)
+		{
+			int msg=0; string a,b;
+			int expected = 50;
+			ifstream in;
+			Flights T;
+			msg = T.start3(a,b,msg);
+			Assert::AreEqual(expected, msg);
+		}
+		TEST_METHOD(datadep)
+		{
+			string a;
+			string expected = "MOSCOW";
+			Flights T;
+			a = T.start2(a);
+			Assert::AreEqual(expected, a);
+		}
+	};
+}
